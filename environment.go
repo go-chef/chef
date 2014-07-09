351f396282d526d604fa564faf645f18ea9b228e
Kraig Amador <kraig@bigkraig.com>
2014-07-09 09:38:28 -0700

    New Service model
    * added for Node
    * added for Environment
    * refactor http for new client access method.
      MakeRequest no longer Do's the request, this will allow our other functions a chance to modify the request before its made, we probably want to rename this to NewRequest

--- environment.go
+++ environment.go
@@ -1,29 +1,55 @@
 package chef
 
-import (
-	"github.com/mitchellh/mapstructure"
-)
+import "fmt"
 
 // Environment has a Reader, hey presto
+type EnvironmentService struct {
+	client Client
+}
+
+// Environment represents the native Go version of the deserialized Environment type
 type Environment struct {
-	*Reader
-	*nativeEnvironment
+	Name               string      `json:"name"`
+	Description        string      `json:"description"`
+	ChefType           string      `json:"chef_type"`
+	DefaultAttributes  interface{} `json:"default_attributes"`
+	OverrideAttributes interface{} `json:"override_attributes"`
+	JsonClass          string      `json:"json_class"`
+	CookbookVersions   interface{} `json:"cookbook_versions"`
 }
 
-// NativeEnvironment represents the native Go version of the deserialized Environment type
-type nativeEnvironment struct {
-	Name             string                    `mapstructure:"name"`
-	Environment      string                    `mapstructure:"chef_environment"`
-	Default          map[string]interface{}    `mapstructure:"default_attributes"`
-	Override         map[string]interface{}    `mapstructure:"override_attributes"`
-	CookbookVersions map[string]nativeCookbook `mapstructure:"cookbook_versions"`
+// List lists the environments in the Chef server.
+//
+// Chef API docs: http://docs.opscode.com/api_chef_server.html#id14
+func (e *EnvironmentService) List() (data map[string]string, err error) {
+	req, err := e.client.MakeRequest("GET", "environments", nil)
+	if err != nil {
+		return
+	}
+
+	_, err = e.client.Do(req, &data)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
-// NewEnvironment wraps a Environment around a pointer to a Reader
-func NewEnvironment(reader *Reader) (*Environment, error) {
-	environment := Environment{reader, &nativeEnvironment{}}
-	if err := mapstructure.Decode(reader, environment.nativeEnvironment); err != nil {
+// Get gets an environment from the Chef server.
+//
+// Chef API docs: http://docs.opscode.com/api_chef_server.html#id17
+func (e *EnvironmentService) Get(name string) (*Environment, error) {
+	url := fmt.Sprintf("environments/%s", name)
+	req, err := e.client.MakeRequest("GET", url, nil)
+	if err != nil {
 		return nil, err
 	}
-	return &environment, nil
+
+	environment := new(Environment)
+	_, err = e.client.Do(req, &environment)
+	if err != nil {
+		return nil, err
+	}
+
+	return environment, nil
 }

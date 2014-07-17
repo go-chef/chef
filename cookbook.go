838e50e8ecb3e0576d0f23af00da5df1f36dc1b9
Jesse Nelson <spheromak@gmail.com>
2014-07-16 20:19:33 -0700

    inintial work on cookbook

--- cookbook.go
+++ cookbook.go
@@ -1,70 +1,93 @@
 package chef
 
-import (
-	"github.com/mitchellh/mapstructure"
-)
-
-// Cookbook is the chef-cookbook container
-type Cookbook struct {
-	*Reader
-	*nativeCookbook
+type CookbookService struct {
+	client *Client
 }
 
 // Each Cookbook lists it's files as a cookbookItem. This structure captures those and makes it easier to work with cook data
 type CookbookItem struct {
-	Url         string `mapstructure:"url"`
-	Path        string `mapstructure:"path"`
-	Name        string `mapstructure:"name"`
-	Checksum    string `mapstructure:"checksum"`
-	Specificity string `mapstructure:"specificity"`
+	Url         string `json:"url"`
+	Path        string `json:"path"`
+	Name        string `json:"name"`
+	Checksum    string `json:"checksum"`
+	Specificity string `json:"specificity"`
+}
+
+// CookbookListResult is the summary info returned by chef-api when listing
+// http://docs.opscode.com/api_chef_server.html#cookbooks
+// {
+//  "apache2" => {
+//    "url" => "http://localhost:4000/cookbooks/apache2",
+//    "versions" => [
+//      {"url" => "http://localhost:4000/cookbooks/apache2/5.1.0",
+//       "version" => "5.1.0"},
+//      {"url" => "http://localhost:4000/cookbooks/apache2/4.2.0",
+//       "version" => "4.2.0"}
+//    ]
+//  }
+//}
+type CookbookListResult struct {
+	Cookbooks map[string]CookbookVersion
+}
+
+type CookbookVersion struct {
+	Url      string
+	Versions map[string]CookbookVersion `json:"version"`
 }
 
 // CookbookMeta represents a Golang version of cookbook metadata
 type CookbookMeta struct {
-	Name            string                 `mapstructure:"cookbook_name"`
-	Version         string                 `mapstructure:"version"`
-	Description     string                 `mapstructure:"description"`
-	LongDescription string                 `mapstructure:"long_description"`
-	Maintainer      string                 `mapstructure:"maintainer"`
-	MaintainerEmail string                 `mapstructure:"maintainer_email"`
-	License         string                 `mapstructure:"license"`
-	Platforms       map[string]string      `mapstructure:"platforms"`
-	Depends         map[string]string      `mapstructure:"dependencies"`
-	Reccomends      map[string]string      `mapstructure:"recommendations"`
-	Suggests        map[string]string      `mapstructure:"suggestions"`
-	Conflicts       map[string]string      `mapstructure:"conflicting"`
-	Provides        map[string]string      `mapstructure:"providing"`
-	Replaces        map[string]string      `mapstructure:"replacing"`
-	attributes      map[string]interface{} `mapstructure:"attributes"` // this has a format as well that could be typed, but blargh https://github.com/lob/chef/blob/master/cookbooks/apache2/metadata.json
-	groupings       map[string]interface{} `mapstructure:"groupings"`  // never actually seen this used.. looks like it should be map[string]map[string]string, but not sure http://docs.opscode.com/essentials_cookbook_metadata.html
-	recipes         map[string]string      `mapstructure:"recipes"`
+	Name            string                 `json:"cookbook_name"`
+	Version         string                 `json:"version"`
+	Description     string                 `json:"description,omitempty"`
+	LongDescription string                 `json:"long_description,omitempty"`
+	Maintainer      string                 `json:"maintainer,omitempty"`
+	MaintainerEmail string                 `json:"maintainer_email"`
+	License         string                 `json:"license"`
+	Platforms       map[string]string      `json:"platforms"`
+	Depends         map[string]string      `json:"dependencies"`
+	Reccomends      map[string]string      `json:"recommendations"`
+	Suggests        map[string]string      `json:"suggestions"`
+	Conflicts       map[string]string      `json:"conflicting"`
+	Provides        map[string]string      `json:"providing"`
+	Replaces        map[string]string      `json:"replacing"`
+	attributes      map[string]interface{} `json:"attributes"` // this has a format as well that could be typed, but blargh https://github.com/lob/chef/blob/master/cookbooks/apache2/metadata.json
+	groupings       map[string]interface{} `json:"groupings"`  // never actually seen this used.. looks like it should be map[string]map[string]string, but not sure http://docs.opscode.com/essentials_cookbook_metadata.html
+	recipes         map[string]string      `json:"recipes"`
 }
 
 // NativeNode represents the native Go version of the deserialized cookbook
-type nativeCookbook struct {
-	Name         string         `mapstructure:"name"`
-	Version      string         `mapstructure:"version"`
-	ChefType     string         `mapstructure:"chef_type"`
-	Frozen       bool           `mapstructure:"frozen?"`
-	JsonClass    string         `mapstructure:"json_class"`
-	CookbookName string         `mapstructure:"cookbook_name"` // yes the json can have this :\
-	Files        []CookbookItem `mapstructure:"files"`
-	Templates    []CookbookItem `mapstructure:"Templates"`
-	Attributes   []CookbookItem `mapstructure:"attributes"`
-	Recipes      []CookbookItem `mapstructure:"recipes"`
-	Definitions  []CookbookItem `mapstructure:"definitions"`
-	Libraries    []CookbookItem `mapstructure:"libraries"`
-	Providers    []CookbookItem `mapstructure:"Providers"`
-	Resources    []CookbookItem `mapstructure:"Resources"`
-	RootFiles    []CookbookItem `mapstructure:"Templates"`
-	Metadata     CookbookMeta   `mapstructure:"Metadata"`
+type Cookbook struct {
+	Name         string         `json:"name"`
+	Version      string         `json:"version"`
+	ChefType     string         `json:"chef_type"`
+	Frozen       bool           `json:"frozen?"`
+	JsonClass    string         `json:"json_class"`
+	CookbookName string         `json:"cookbook_name"` // yes the json can have this :\
+	Files        []CookbookItem `json:"files"`
+	Templates    []CookbookItem `json:"Templates"`
+	Attributes   []CookbookItem `json:"attributes"`
+	Recipes      []CookbookItem `json:"recipes"`
+	Definitions  []CookbookItem `json:"definitions"`
+	Libraries    []CookbookItem `json:"libraries"`
+	Providers    []CookbookItem `json:"Providers"`
+	Resources    []CookbookItem `json:"Resources"`
+	RootFiles    []CookbookItem `json:"Templates"`
+	Metadata     CookbookMeta   `json:"Metadata"`
 }
 
-// NewCookbook is used to create a cookbook from a Reader
-func NewCookbook(reader *Reader) (*Cookbook, error) {
-	cook := Cookbook{reader, &nativeCookbook{}}
-	if err := mapstructure.Decode(reader, cook.nativeCookbook); err != nil {
-		return nil, err
-	}
-	return &cook, nil
+// Get - Grabs a cookbook from a chef api and populates the object
+func (c *CookbookService) Get(name string, numVersions int) (CookbookVersion, error) {
+	return CookbookVersion{}, nil
+}
+
+// GetVersion
+// /cookbook/foo/1.2.3
+// /cookbook/foo/_latest
+func (c *CookbookService) GetVersion(name string, version string) {
+}
+
+// Get - Grabs a cookbook from a chef api and populates the object
+func (c *CookbookService) List() (CookbookListResult, error) {
+	return CookbookListResult{}, nil
 }

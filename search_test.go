829327c82aa3b95fcd41fbdbe10836ad694e6a37
Mudassar Shafique <15931574+mudash@users.noreply.github.com>
2020-05-28 11:43:32 -0400

    added test case for multiple http calls via partial search
    Signed-off-by: Mudassar Shafique <15931574+mudash@users.noreply.github.com>

--- search_test.go
+++ search_test.go
@@ -2,12 +2,16 @@ package chef
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+const partialSearchResponseFile_1 = "test/partial_search_test_1.json"
+const partialSearchResponseFile_2 = "test/partial_search_test_2.json"
 
 func TestSearch_Get(t *testing.T) {
 	setup()
@@ -128,5 +132,53 @@ func TestSearch_PartialExec(t *testing.T) {
 	}
 
 	assert.Equal(t, "grafana", pres.Rows[0].(map[string]interface{})["policy_name"])
+
+}
+
+func TestSearch_PartialExecMultipleCalls(t *testing.T) {
+	setup()
+	defer teardown()
+
+	searchResponseOne, err := ioutil.ReadFile(partialSearchResponseFile_1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	searchResponseTwo, err := ioutil.ReadFile(partialSearchResponseFile_2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	mux.HandleFunc("/search/node", func(w http.ResponseWriter, r *http.Request) {
+
+		start, ok := r.URL.Query()["start"]
+
+		if !ok || len(start[0]) < 1 {
+			fmt.Println("Url Param 'start' is missing")
+			return
+		}
+
+		if start[0] == "0" {
+			fmt.Fprintf(w, string(searchResponseOne))
+		} else {
+			fmt.Fprintf(w, string(searchResponseTwo))
+		}
+	})
+
+	query := map[string]interface{}{
+		"name":            []string{"name"},
+		"policy_group":    []string{"policy_group"},
+		"policy_name":     []string{"policy_name"},
+		"policy_revision": []string{"policy_revision"},
+	}
+
+	pres, err := client.Search.PartialExec("node", "*.*", query)
+	if err != nil {
+		t.Errorf("Search.PartialExec failed err: %+v", err)
+	}
+
+	assert.Equal(t, 1185, len(pres.Rows))
+	assert.Equal(t, "node1", pres.Rows[0].(map[string]interface{})["name"])
+	assert.Equal(t, "node1185", pres.Rows[len(pres.Rows)-1].(map[string]interface{})["name"])
 
 }

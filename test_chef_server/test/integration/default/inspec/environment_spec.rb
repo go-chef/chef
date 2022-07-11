29e0236226d0c4d8748373c9b5b20e51096897d3
MarkGibbons <gibbonsmj80@gmail.com>
2022-07-11 10:24:53 -0700

    Fix integration tests

---
+++ test_chef_server/test/integration/default/inspec/environment_spec.rb
@@ -0,0 +1,9 @@
+# Inspec tests for the cookbook chef api go module
+#
+describe command('/go/src/testapi/bin/environment') do
+  its('stderr') { should_not match(/error|no such file|cannot find|not used|undefined/) }
+  its('stderr') { should_not match(/testbook/) }
+  its('stderr') { should_not match(/sampbook/) }
+  its('stderr') { should_not match(/Issue loading/) }
+  its('stdout') { should match(%r{^List initial environments\s*$}) }
+end
--- test_chef_server/test/integration/default/inspec/license.rb
+++ test_chef_server/test/integration/default/inspec/license.rb
@@ -4,5 +4,5 @@
 describe command('/go/src/testapi/bin/license') do
   its('stderr') { should_not match(/error|no such file|cannot find|not used|undefined/) }
   its('stderr') { should_not match(/Issue/) }
-  its('stdout') { should match(%r{^List license: {LimitExceeded:false NodeLicense:25 NodeCount:0 UpgradeUrl:http://www.chef.io/pricing}}) }
+  its('stdout') { should match(%r{^List license: {LimitExceeded:false NodeLicense:25 NodeCount:0 UpgradeUrl:https://www.chef.io/pricing}}) }
 end
